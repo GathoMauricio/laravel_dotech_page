6975fbcc835a367d713ebc8769e299b1232e5854
Mauricio Soriano <mauricio2769@gmail.com>
2021-08-13 00:14:16 -0500

    probar multiples emails

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -10,8 +10,8 @@ class ContactoController extends Controller
     {
         \Mail::send('email.contacto', ['request' => $request], function ($mail) use ($request) {
             $mail->from('dotechlanding@dotredes.com',env('APP_NAME'));
-            $mail->to('mauricio2769@gmail.com');
-            //$mail->to('contacto@dotredes.com');
+            $mail->to(['mauricio2769@gmail.com','mauricio2769@outlook.es']);
+            //$mail->to(['contacto@dotredes.com','rortuno@dotredes.com']);
         });
         return redirect()->back()->with('contacto','success');
     }
