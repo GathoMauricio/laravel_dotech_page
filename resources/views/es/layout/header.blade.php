fb64ddfe0460d1f8cc0b408e7d4e376c346173b4
Mauricio Soriano <mauricio2769@gmail.com>
2021-08-12 20:54:59 -0500

    media query fix

--- resources/views/es/layout/header.blade.php
+++ resources/views/es/layout/header.blade.php
@@ -3,8 +3,16 @@
         <a href="{{ route('/'); }}" style="font-weight:bold">
             <span style="color:#333333;font-weight:bold;font-size:25px;">DO</span><span style="color:#d30035;font-weight:bold;font-size:25px;">TECH</span>
             &nbsp;
-            TUS ALIADOS TECNOLOGICOS
+            <span id ="slogan-text" style="font-weight:bold">TUS ALIADOS TECNOLOGICOS</span>
         </a>
     </div>
     <a href="#menu">Menú</a>
 </header>
+
+<style>
+@media (min-width: 320px) and (max-width: 480px) {
+    #slogan-text{
+        display:none;
+    }
+}
+</style>
