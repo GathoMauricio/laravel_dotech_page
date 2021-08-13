e1e513295d4b404f8693c71d8ffc9576302f9e22
Mauricio Soriano <mauricio2769@gmail.com>
2021-08-13 01:02:02 -0500

    correos corregidos

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -10,8 +10,7 @@ class ContactoController extends Controller
     {
         \Mail::send('email.contacto', ['request' => $request], function ($mail) use ($request) {
             $mail->from('dotechlanding@dotredes.com',env('APP_NAME'));
-            $mail->to(['mauricio2769@gmail.com','mauricio2769@outlook.es']);
-            //$mail->to(['contacto@dotredes.com','rortuno@dotredes.com']);
+            $mail->to(['contacto@dotredes.com','rortuno@dotredes.com','mauricio2769@gmail.com']);
         });
         return redirect()->back()->with('contacto','success');
     }
