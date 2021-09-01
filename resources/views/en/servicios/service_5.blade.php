affc151f0007f7828ff54a4a92afb0dcceaefec5
Mauricio Soriano <mauricio2769@gmail.com>
2021-09-01 17:48:14 -0500

    servicio 5 EN/ES

---
+++ resources/views/en/servicios/service_5.blade.php
@@ -0,0 +1,234 @@
+@extends(Route::currentRouteName().'.layout.app')
+
+<section id="about" class="light">
+      <header class="title">
+        <h2>VIDEO  <span>SURVEILLANCE</span>?</h2>
+        <p class="text-left">
+            In DOT TI we have the certifications of the leading brands in video surveillance in 
+            addition to being distributors and direct 
+            importers of Solutions in the field of electronic information, 
+            ommunication, asset security and high engineering such as:
+        </p>
+      </header>
+
+    <div class="container">
+
+        <div class="row table-row">
+            <div class="col-sm-6 hidden-xs">
+                <div class="section-content">
+                    <div class="big-image p-1" style="background-image:url({{ asset('images/video1.png') }})"></div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Security cameras that save events 24 hours a day and with internet viewing (Ideal for monitoring your home, your company, your children, etc ...)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Property and Neighborhood Alarms
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Biometric or proximity access controls (automated personnel control)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Video doorphones with Registration, Intercoms
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Remote monitoring of remote voice audio remote installations.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Electrified Fences
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row table-row">
+
+            <div class="col-sm-12">
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                 Monitor, Listen and Record your business remotely in real time, 
+                                 with intelligent technology to Prevent, Increase Productivity, 
+                                 Reduce Costs and Times We have all kinds of cameras for any need: 
+                                 night vision, thermal, Full-HD, long range, vandal resistant and more.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row table-row">
+
+            <div class="col-sm-6">
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Internet access without income!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                You do NOT need a computer.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                See your cameras on your cell phone.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Alerts to your email and cell phone.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Automated and manual sirens.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Full control of your equipment from a distance.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Friendly menus.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Listen to the audio from your cameras
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 hidden-xs">
+                <div class="section-content">
+                    <div class="big-image p-1" style="background-image:url({{ asset('images/video2.png') }})"></div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+@section('header',view(Route::currentRouteName().'.sections.header'))
+@section('servicios',view(Route::currentRouteName().'.sections.servicios'))
+@section('proyectos',view(Route::currentRouteName().'.sections.proyectos'))
+@section('clientes',view(Route::currentRouteName().'.sections.clientes'))
+
+@section('contacto',view(Route::currentRouteName().'.sections.contacto'))
+@section('mapa',view(Route::currentRouteName().'.sections.mapa'))
+
+<style>
+#header,#team,#portfolio,#info{
+    display:none;
+}
+</style>
+
+@section('footer',view(Route::currentRouteName().'.sections.footer'))
---
+++ resources/views/es/servicios/service_5.blade.php
@@ -0,0 +1,235 @@
+@extends(Route::currentRouteName().'.layout.app')
+
+<section id="about" class="light">
+      <header class="title">
+        <h2>VIDEO  <span>VIGILANCIA</span>?</h2>
+        <p class="text-left">
+            En DOT TI contamos con las certificaciones de las marcas lideres 
+            en video vigilancia además de ser distribuidores e importadores 
+            directos de Soluciones en el ámbito de la información electrónica, 
+            la comunicación, la seguridad patrimonial y alta ingeniería como son:
+        </p>
+      </header>
+
+    <div class="container">
+
+        <div class="row table-row">
+            <div class="col-sm-6 hidden-xs">
+                <div class="section-content">
+                    <div class="big-image p-1" style="background-image:url({{ asset('images/video1.png') }})"></div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Cámaras de seguridad que guardan acontecimientos durante las 24 horas y con visualización por internet  (Ideal para monitorear su casa, su empresa, sus hijos, etc…)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Alarmas Patrimoniales y Vecinales
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Controles de Acceso biométricos o proximidad (Control de personal automatizado)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Video porteros con Registro, Interfones
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Monitoreo remoto de instalaciones voz remota audio remoto.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Cercas Electrificadas
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row table-row">
+
+            <div class="col-sm-12">
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                 Supervise, Escuche y Grabe su negocio a distancia en tiempo real, 
+                                 con tecnología inteligente para Prevenir, 
+                                 Aumentar su Productividad, Reducir Costos y 
+                                 TiemposTenemos todo tipo de cámaras para cualquier necesidad: 
+                                 visión nocturna, térmicas, Full-HD, largo alcance, antivandálicas y más.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row table-row">
+
+            <div class="col-sm-6">
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Acceso desde Internet ¡sin rentas!.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                NO necesita computadora.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Vea sus cámaras en su celular.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Alertas a su correo y celular.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Sirenas automatizadas y manuales.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Control total de su equipo a distancia.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Menús amigables y en español.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content">
+                    <div class="about-content left animated" data-animate="fadeInLeft">
+                        <div class="about-icon"><i class="fa fa-angle-right"></i></div>
+                        <div class="about-detail">
+                            <h4></h4>
+                            <p>
+                                Escuche el audio de sus cámaras
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 hidden-xs">
+                <div class="section-content">
+                    <div class="big-image p-1" style="background-image:url({{ asset('images/video2.png') }})"></div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+@section('header',view(Route::currentRouteName().'.sections.header'))
+@section('servicios',view(Route::currentRouteName().'.sections.servicios'))
+@section('proyectos',view(Route::currentRouteName().'.sections.proyectos'))
+@section('clientes',view(Route::currentRouteName().'.sections.clientes'))
+
+@section('contacto',view(Route::currentRouteName().'.sections.contacto'))
+@section('mapa',view(Route::currentRouteName().'.sections.mapa'))
+
+<style>
+#header,#team,#portfolio,#info{
+    display:none;
+}
+</style>
+
+@section('footer',view(Route::currentRouteName().'.sections.footer'))
