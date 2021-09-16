f5b252df7d3a1abff610e6320b34146b1f5a99bd
Mauricio Soriano <mauricio2769@gmail.com>
2021-09-16 14:39:08 -0500

    agregar más imágenes a la zona de clientes /EN

--- resources/views/en/sections/clientes.blade.php
+++ resources/views/en/sections/clientes.blade.php
@@ -43,21 +43,260 @@
           </figure>
 
           <figure>
-            <a href="{{ asset('gallery/img/clientes/simi.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/simi.png') }}" alt="#"/></a>
+            <a href="{{ asset('gallery/img/clientes/izzi.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/izzi.png') }}" alt="#"/></a>
             <figcaption>
                 <h2 class="photostack-title">
-                  <span>Farmacias Similares</span>
+                  <span>IZZI Telecom</span>
                   <br><br>
-                  <a href ="https://www.facebook.com/farmaciassimilares" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
-                  <a href ="https://twitter.com/fsimilares" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
-                  <a href ="https://www.youtube.com/user/farmaciassimilaresmx" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
-                  <a href ="https://www.instagram.com/fsimilares/" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
                 </h2>
                 <div class="photostack-back">
-                    <p>Largest pharmaceutical company in Latin America.</p>
+                    <p>description</p>
                 </div>
             </figcaption>
           </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/comercial_mexicana.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/comercial_mexicana.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>Comercial Mexicana</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/gnp.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/gnp.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>GNP Seguros</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/reforma.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/reforma.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>Grupo Reforma</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/diestro.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/diestro.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>DIESTRO</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/movistar.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/movistar.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>MOVISTAR</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/once_tv.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/once_tv.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>ONCE TV</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/penafiel.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/penafiel.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>Peñafiel</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/imss.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/imss.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>IMSS</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/3b.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/3b.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>Tiendas 3B</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/conuee.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/conuee.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>CONUEE</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/lead_image.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/lead_image.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>Lead Image</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/holiday_inn.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/holiday_inn.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>Holiday Inn</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/simex.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/simex.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>SIMEX</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
+          <figure>
+            <a href="{{ asset('gallery/img/clientes/roche.png') }}" class="photostack-img" target="_blank"><img src="{{ asset('gallery/img/clientes/roche.png') }}" alt="#"/></a>
+            <figcaption>
+                <h2 class="photostack-title">
+                  <span>ROCHE</span>
+                  <br><br>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-facebook" style ="color:#3b5998;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-twitter" style ="color:#00ACEE;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-youtube" style ="color:#C4302B;"></span></a>
+                  <a href ="#" target="_blank" style="font-size:24px;"><span class="icon-instagram" style ="color: #C13584;"></span></a>
+                </h2>
+                <div class="photostack-back">
+                    <p>description</p>
+                </div>
+            </figcaption>
+          </figure>
+          
 
       </div>
   </section>
@@ -84,8 +323,8 @@
                   navs[index + 1].click();
                 }
                 setPhotostackItem();
-              },10000);
-            },10000);
+              },5000);
+            },5000);
          }
         });
       }
